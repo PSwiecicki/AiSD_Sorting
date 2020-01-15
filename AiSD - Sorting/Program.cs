5420e966d3650d2726a0a5d95e865e83b08af9a6
Patryk Święcicki <patswiecicki@gmail.com>
2020-01-15 19:16:54 +0100

    Dodaj pliki projektów.

---
+++ AiSD - Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AiSD_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
