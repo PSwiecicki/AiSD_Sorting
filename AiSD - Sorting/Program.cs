217faa762170b086dde646ba273b589c822256c8
Patryk Święcicki <patswiecicki@gmail.com>
2020-01-16 00:26:35 +0100

    Added bubble sort function for table with double variables and modified for faster finish when table is sorted.

--- AiSD - Sorting/Program.cs
+++ AiSD - Sorting/Program.cs
@@ -6,14 +6,14 @@ namespace AiSD_Sorting
     {
         static void Main()
         {
-            int[] testTable = new int[10];
+            double[] testTable = new double[10];
             Random rand = new Random();
             for (int i = 0; i < 10; i++)
             {
-                testTable[i] = rand.Next(0, 101);
+                testTable[i] = rand.NextDouble() * rand.Next(0, 101);
             }
             ShowTable(testTable);
-            SelectionSort(testTable);
+            BubbleSort(testTable);
         }
 
         static int[] BubbleSort(int[] table)
@@ -75,9 +75,34 @@ namespace AiSD_Sorting
                     table[tmpIndex] = table[i];
                     table[i] = tmp;
                 }
-                ShowTable(table);
+                //ShowTable(table);
             }
+            return table;
+        }
 
+        static double[] BubbleSort(double[] table)
+        {
+            double tmp;
+            bool isSorted;
+            for (int i = 0; i < table.Length - 2; i++)
+            {
+                isSorted = true;
+                for (int j = table.Length - 1; j > i; j--)
+                {
+                    if (table[j] < table[j - 1])
+                    {
+                        isSorted = false;
+                        tmp = table[j - 1];
+                        table[j - 1] = table[j];
+                        table[j] = tmp;
+                    }
+                }
+                //ShowTable(table);
+                if (isSorted)
+                {
+                    break;
+                }
+            }
             return table;
         }
 
@@ -85,6 +110,13 @@ namespace AiSD_Sorting
         {
             for (int i = 0; i < table.Length; i++)
                 Console.Write("{0} ", table[i]);
+            Console.WriteLine();
+        }
+
+        static void ShowTable(double[] table)
+        {
+            for (int i = 0; i < table.Length; i++)
+                Console.Write("{0} ", table[i].ToString("F2"));
             Console.WriteLine();
         }
     }
